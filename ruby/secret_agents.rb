5d092c5e49fdf4cac8b83e08b83eb6ccb31725d1
Matthew Fischer <matthew.gfischer@gmail.com>
2017-01-26 15:22:07 -0800

    Add method pseudocode

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,25 @@
+# * Encrypt *
+# - Receive string as input variable
+# - For each letter that is in input string:
+#   - If letter = "z"
+#     - Reassign to "a"
+#   - Elsif character is " " (space)
+#     - No character reassignment
+#   - Else
+#     - Reassign to next letter in the alphabet
+# - Return new string
+
+# * Decrypt *
+# - Receive string as input variable
+# - For each character that is in input string:
+#   - If character = "a"
+#     - Reassign to "z"
+#   - Elsif character is " " (space)
+#     - No character reassignment
+#   - Else
+#     - Reassign to previous letter in alphabet
+#       - Find index in alphabet
+#       - Subtract 1 from index
+#       - Get letter that corresponds to new index
+# - Return new string 
+
