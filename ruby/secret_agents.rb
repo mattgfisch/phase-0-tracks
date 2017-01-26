b34154206c55e6110ee8217d553fe7331f8d263b
Matthew Fischer <matthew.gfischer@gmail.com>
2017-01-26 15:45:35 -0800

    Add methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,6 +9,20 @@
 #     - Reassign to next letter in the alphabet
 # - Return new string
 
+def encrypt(in_string)
+  for i in 0..(in_string.length - 1)
+    if in_string[i] == "z"
+      in_string[i] = "a"
+    elsif in_string[i] == " " 
+    else
+      in_string[i] = in_string[i].next
+    end
+  end
+  in_string  
+end
+
+
+
 # * Decrypt *
 # - Receive string as input variable
 # - For each character that is in input string:
@@ -23,3 +37,19 @@
 #       - Get letter that corresponds to new index
 # - Return new string 
 
+def decrypt(in_string)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  for i in 0..(in_string.length - 1)
+    if in_string[i] == "a"
+      in_string[i] = "z"
+    elsif in_string[i] == " " 
+    else
+      in_string[i] = alphabet[alphabet.index(in_string[i]) - 1]
+    end
+  end
+  in_string  
+end
+
+
+# puts decrypt(encrypt("swordfish"))  # The return of the interior method supplies the 
+                                    # exterior method with its argument
