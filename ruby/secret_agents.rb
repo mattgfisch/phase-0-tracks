64b3e7169f569ca0fbca316aac0f3acbae717986
Matthew Fischer <matthew.gfischer@gmail.com>
2017-01-26 16:31:12 -0800

    Add interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -53,3 +53,54 @@ end
 
 # puts decrypt(encrypt("swordfish"))  # The return of the interior method supplies the 
                                     # exterior method with its argument
+
+
+# DRIVER CODE
+response = ""
+password = ""
+valid_response = false
+
+# Asks user whether they would like to decrypt or encrypt a password
+# Repeat until users gives valid response (encrypt or decrypt)
+puts "Would you like to decrypt or encrypt a password?"
+
+until valid_response
+  response = gets.chomp
+  
+  if response == "encrypt" || response == "decrypt"
+    valid_response = true
+  end
+
+  if !valid_response
+    puts "Please respond with either 'decrypt' or 'encrypt'."
+  end
+end
+
+
+# Reset valid response for repeat use
+valid_response = false
+
+# Asks user for the password
+# Repeat until users gives valid response (a non-empty string)
+puts "What is the password you would like to #{response}?"
+
+until valid_response
+  password = gets.chomp
+  if !(password.empty?) 
+    valid_response = true
+  end
+
+  if !valid_response
+    puts "Please respond with a non-empty string."
+  end
+end
+
+# Conduct encrypt or decrypt, based on response
+if response == "encrypt"
+  puts "Encrypted password: #{encrypt(password)}"
+else
+  puts "Decrypted password: #{decrypt(password)}"
+end
+
+
+
