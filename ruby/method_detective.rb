2a91527e86e2ca97715cb718bff894d34f743fc0
Matthew Fischer <matthew.gfischer@gmail.com>
2017-01-30 20:52:23 -0800

    Add missing methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,44 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts "zom".replace("zoom")
+puts "zom".insert(1, "o")
+puts "zom".insert(2, "o")
 # => “zoom”
 
-# "enhance".<???>
+puts "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+ puts "Stop! You’re under arrest!".upcase
+ puts "Stop! You’re under arrest!".upcase!
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".concat(" suspects")
+puts "the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+puts " suspects".insert(0, "the usual")
+#=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+puts "The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chop!
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".delete("T", "Th")
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
+puts "Elementary,    my   dear        Watson!".squeeze!(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".each_byte{|c| print c, ' '}
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# >> 122 is the decimal code value for the character "z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
