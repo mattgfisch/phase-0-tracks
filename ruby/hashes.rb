8608de24a99c163cf1b195180233843f34dd5d74
Matthew Fischer <matthew.gfischer@gmail.com>
2017-01-31 23:19:25 -0800

    Add prompts and print hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,4 +20,33 @@
 #       - Based on answer, prompt for new value for given key
 # - Print new hash
    
+client_details = Hash.new
+
+puts "What is your client's name?"
+full_name = gets.chomp
+client_details.store(:name, full_name)
+
+puts "What is your client's age?"
+age = gets.chomp.to_i
+client_details.store(:age, age)
+
+puts "How many children does your client have?"
+num_children = gets.chomp.to_i
+client_details.store(:num_children, num_children)
+
+puts "What theme would your client like to have?"
+theme = gets.chomp
+client_details.store(:theme, theme)
+
+puts "Would your client like to spend over $10,000? (y/n)"
+over_10_grand = gets.chomp
+if over_10_grand == "y"
+  over_10_grand = true
+else
+  over_10_grand = false
+end
+client_details.store(:over_10_grand, over_10_grand)
+
+p client_details
+
 
