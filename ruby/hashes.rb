2bca424e2497010e6727185228ecef9cf6b5a0c1
Matthew Fischer <matthew.gfischer@gmail.com>
2017-02-01 00:40:33 -0800

    Add possibility to change key value

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -20,8 +20,18 @@
 #       - Based on answer, prompt for new value for given key
 # - Print new hash
    
+#
+#
+#
+# DRIVER CODE
+#
+#
+#
+
+# Create new hash
 client_details = Hash.new
 
+# Prompt user for information, store with corresponding key
 puts "What is your client's name?"
 full_name = gets.chomp
 client_details.store(:name, full_name)
@@ -47,6 +57,42 @@ else
 end
 client_details.store(:over_10_grand, over_10_grand)
 
+
+# Print hash
+p client_details
+
+# Prompt if user wants to change any information
+# If they do, prompt for value for given decor
+puts "Which key would you like to change? If none, type 'none'."
+key = gets.chomp.to_sym
+
+if key == :none
+# If 'none' is response, then skip to end
+
+else
+  puts "What would you like it changed to?"
+  puts "It's currently: #{client_details[key]}"
+  new_value = gets.chomp     #Get new value
+
+  value_class = client_details[key].class #Stores class value to variable
+ 
+  if value_class == "string".class #Take action based on class of target value
+    new_value
+  elsif value_class == 1.class
+    new_value.to_i
+  elsif value_class == true.class || value_class == false.class
+    if new_value == "true"
+      new_value = true
+    else
+      new_value = false
+    end
+  end   
+
+  client_details.store(key, new_value) #Store new value in hash
+end
+
+
+puts "**********NEW HASH***********"
 p client_details
 
 
