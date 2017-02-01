281ee124e33fb2f834835147842d0bdd73f729b7
Matthew Fischer <matthew.gfischer@gmail.com>
2017-01-31 22:50:55 -0800

    Add description and pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,23 @@
+
+# DESCRIPTION: 
+# A program that will allow an interior designer 
+# to enter the details of a given client: the client's name, age, number 
+# of children, decor theme, and so on (you can choose your own as long as 
+# it's a good mix of string, integer, and boolean data).
+
+# PSEUDOCODE:
+# - Create empty hash
+# - Prompt user for information; repeat until all info is in hash
+#   - Ask question
+#   - Take in data
+#   - Convert to proper data type
+#   - Add w/key to hash
+# - Print hash out to screen
+# - Give opportunity to update key
+#   - Prompt for which key
+#     - If response == none, skip
+#     - Else
+#       - Based on answer, prompt for new value for given key
+# - Print new hash
+   
+
