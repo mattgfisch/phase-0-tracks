b0a5cd641399c2ad3b2a23b47f2b765f1b37b035
Matthew Fischer <matthew.gfischer@gmail.com>
2017-02-11 23:58:00 -0800

    Add two module methods and implement test code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!" + " :) :) :)"
+  end
 end
+
+p Shout.yell_angrily("Why I oughtta")
+
+p Shout.yelling_happily("Hooray")
