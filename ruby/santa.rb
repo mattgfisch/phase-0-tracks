3180ba27f2baeb8b6aace2ab018cc4c54ceab3f8
Matthew Fischer <matthew.gfischer@gmail.com>
2017-02-11 21:15:01 -0800

    Add attribute changing and getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -20,15 +20,38 @@ class Santa
     p "That was a good #{cookie_type} cookie!"
   end
 
+  #Attribute changing methods
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking << reindeer
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  #Getter methods
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end  
+
 end
 
 # ----- TEST CODE
 
-happy = Santa.new("male", "white")
+# happy = Santa.new("male", "white")
 
-happy.speak
+# happy.speak
 
-happy.eat_milk_and_cookies("snickerdoodle")
+# happy.eat_milk_and_cookies("snickerdoodle")
 
 #Create array of santas
 santas = []
@@ -42,7 +65,11 @@ test_genders.length.times do |i|
   santas << Santa.new(test_genders[i], test_ethnicities[i])
 end
 
-#Should print data for 6th santa
-p santas[5]
+# #Should print data for 6th santa
+# p santas[5]
+
+p santas[4].celebrate_birthday
+p santas[4].get_mad_at("Dancer")
+p santas[5].gender = "Male"
 
 
