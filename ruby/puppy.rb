aedc95cc9a53dcf00250daf38124548358f1a0a5
Matthew Fischer <matthew.gfischer@gmail.com>
2017-02-11 16:32:21 -0800

    Add basic outline of Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# PUPPY CLASS DESIGN
+=begin
+ 
+ CLASS ---------------
+ Puppy
+
+ CHARACTERISTICS ---------------
+ Size: small
+ Adorable: yes
+ Name: varies
+ Color: varies
+ Leg count: 4
+ Tail count: 1
+
+ BEHAVIOR ---------------
+ Beg
+ Whimper
+ Play
+ Cuddle
+ 
+=end
