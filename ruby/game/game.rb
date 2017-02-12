ec4c45191cf08bcc06ec23068b1e63afa9f45de5
Matthew Fischer <matthew.gfischer@gmail.com>
2017-02-12 15:54:11 -0800

    Initial commit and add frame pseudocode

---
+++ ruby/game/game.rb
@@ -0,0 +1,17 @@
+# ----- PSEUDOCODE
+# Input word
+  # Loop until guess_count > word.length + 3
+    # Display word as spaces + guessed char (guessed_word)
+    # Input guess (char)
+    # Compare guess to word
+    # Check if char already guessed
+        # If so, prompt again
+    # Increment guess_count by 1
+    # Loop through array of word char's
+      # If guess = char in word, modify guessed_word at index
+        # Print correct guess
+      # Else
+        # Print incorrect guess
+# Print either win or lose
+  # Based on calculate_win method and won_game boolean
+
