078bb0caa8f02a14bf6bc4aa825b7a5a2c0591ed
Matthew Fischer <matthew.gfischer@gmail.com>
2017-02-24 20:40:36 -0800

    Add ability to populate quality adjustments table based on teams table

--- databases/basketball_game/season_simulator.rb
+++ databases/basketball_game/season_simulator.rb
@@ -23,7 +23,47 @@ def create_team(db, name, wins, losses)
   db.execute(team_creation, [name, wins, losses])
 end
 
+def create_delta(db, team_id, num_teams) 
+  true_delta = determine_delta(team_id, num_teams)
 
+  delta_creation = <<-SQL
+    INSERT INTO quality_adjustments (delta)
+    VALUES (?)
+  SQL
+
+  db.execute(delta_creation, [true_delta])
+end
+
+def determine_delta(team_id, num_teams)
+  delta = 0
+
+  case team_id
+    when 1
+      delta = 1
+    when 2
+      delta = 2
+    when 3
+      delta = 3
+    when num_teams - 2
+      delta = 1
+    when num_teams - 1
+      delta = 3
+    when num_teams
+      delta = 5
+    else
+      random = rand(3)
+      case random
+        when 0
+          delta = -1
+        when 1
+          delta = 0
+        when 2
+          delta = 1
+      end
+  end
+
+  delta
+end 
 
 
 
@@ -64,6 +104,27 @@ db.execute(create_teams_table)
 db.execute(create_qa_table)
 
 
-# Populate teams table
+# Wipe teams table (if teams exist)
+db.execute("DELETE FROM teams WHERE id>0")
 
-create_team(db, "Knicks", 10, 5)
+
+# Populate teams table
+num_teams = 20
+
+num_teams.times do 
+  create_team(db, "Knicks", 10, 5)
+end
+
+all_teams = db.execute("SELECT * FROM teams")
+
+# Wipe quality adjustments table (if id's exist)
+db.execute("DELETE FROM quality_adjustments WHERE id>0")
+
+
+# Populate quality adjustments table
+all_teams.each do |team|
+  create_delta(db, team[0], all_teams.length)
+end
+
+
+
