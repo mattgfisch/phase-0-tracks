9d23540713562abfcf502dbce7c992713a7421dd
Matthew Fischer <matthew.gfischer@gmail.com>
2017-03-01 21:26:46 -0800

    Complete module

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -5,6 +5,7 @@ require 'sqlite3'
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
+
 db.results_as_hash = true
 
 # show students on the home page
